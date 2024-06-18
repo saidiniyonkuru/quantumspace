f6db426cbae5782f1a9ebc5492eea950a0ac1656
saidiniyonkuru <zealbabu5@gmail.com>
2024-06-18 03:57:07 +0100

    Describe your changes here

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,24 @@
+import {useState} from 'react';
+import axios from 'axios';
 import '../css/App.css';
 import {FaSearch} from 'react-icons/fa';
 
 function Header() {
+  const [showModal, setShowModal] = useState(false);
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+
+  const handleAddTodo = async () => {
+    try {
+      await axios.post('/api/todos', {name, address});
+      setName('');
+      setAddress('');
+      setShowModal(false);
+    } catch (error) {
+      console.error('Error adding todo:', error);
+    }
+  };
+
   return (
     <>
       <div className='header'>
@@ -27,17 +44,49 @@ function Header() {
           </div>
           <div className='list'>
             <li>
-              <a
-                href='add.html'
+              <button
                 className='aj_btn'
+                onClick={() => setShowModal(true)}
               >
                 Ajouter&nbsp;un&nbsp;kvm
-              </a>
+              </button>
             </li>
           </div>
         </div>
       </div>
+
+      {showModal && (
+        <div className='modal'>
+          <div className='modal-content'>
+            <span
+              className='close-button'
+              onClick={() => setShowModal(false)}
+            >
+              &times;
+            </span>
+            <h2>Add a new kvm</h2>
+            <label>
+              Name:
+              <input
+                type='text'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+            <label>
+              Address:
+              <input
+                type='text'
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </label>
+            <button onClick={handleAddTodo}>Add</button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
+
 export default Header;
